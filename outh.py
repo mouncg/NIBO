419fb92bad303aa7e832cda3d6a211b0460b52a1
EPFFORCE <38656336+mouncg@users.noreply.github.com>
2020-04-07 00:01:57 +0100

    try to make work

--- outh.py
+++ outh.py
@@ -4,7 +4,7 @@ class MySQL:
     """
 
     def __init__(self):
-        self.host = "EPPYMYSQL"
+        self.host = "172.17.0.11"
         self.port = 3306  # int
         self.user = "NTT_ACCESS"
         self.password = "VZUs|(c8z{CcvEK7Tm29h]GvVw'Gd!VW"
