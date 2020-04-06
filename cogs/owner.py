1a7faf76bc3060e8e7af447060afce73a335e60d
EPFFORCE <38656336+mouncg@users.noreply.github.com>
2020-04-07 00:27:41 +0100

    try to make work

--- cogs/owner.py
+++ cogs/owner.py
@@ -10,15 +10,22 @@ class OwnerCommands(commands.Cog):
 
     @commands.is_owner()
     @commands.command(name="add_user")
-    async def add_user(self, ctx: commands.Context, user: greedy[discord.User]):
+    async def add_user(self, ctx: commands.Context, users: greedy[discord.User]):
+        results = []
         async with self.bot.pool.acquire() as conn:
             async with conn.cursor() as cur:
-                await cur.execute(
-                    f"insert into whitelisted_users values ('{user[0].id}')"
-                )
-                results = await cur.fetchone()
+
+                for user in users:
+                    await cur.execute(
+                        f"insert into whitelisted_users values ('{user.id}')"
+                    )
+                    res = await cur.fetchone()
+                    results.append(res)
         if f"{results}" == f"None":
-            results = f"{user[0]} has been added to the whitelisted users config!"
+            userStr = ""
+            for _ in users:
+                userStr += f"{_},"
+            results = f"{userStr} has been added to the whitelisted users!"
         return await ctx.send(f"✅| {results}")
 
 
