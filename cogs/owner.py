78f75b05539767833e23d2e0300c6776827612f6
EPFFORCE <38656336+mouncg@users.noreply.github.com>
2020-04-07 00:20:59 +0100

    try to make work

--- cogs/owner.py
+++ cogs/owner.py
@@ -14,7 +14,7 @@ class OwnerCommands(commands.Cog):
         print(user)
         async with self.bot.pool.acquire() as conn:
             async with conn.cursor() as cur:
-                await cur.execute(f"insert into whitelisted_users value {user[0].id}")
+                await cur.execute(f"insert into whitelisted_users value '{user[0].id}'")
                 results = await cur.fetchone()
         return await ctx.send(f"✅| {results}")
 
