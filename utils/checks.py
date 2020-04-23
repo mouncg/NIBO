97a9bef315ef07a94b56ea89d916ca07668a59fd
EPFFORCE <38656336+mouncg@users.noreply.github.com>
2020-04-23 18:26:01 +0100

    ree

--- utils/checks.py
+++ utils/checks.py
@@ -1,6 +1,21 @@
 import json
+
+import asyncio
 from discord.ext import commands
 from main import NitroBot as bot
+
+
+async def select(bot: bot, sql, all=False):
+    while not bot.pool:
+        await asyncio.sleep(0.21)
+    async with bot.pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute(
+                f"SELECT " + sql if not str(sql).lower().startswith("select") else sql
+            )
+            if all:
+                return await cur.fetchall()
+            return await cur.fetchone()
 
 
 def dat():
@@ -11,12 +26,12 @@ def dat():
     return x, y
 
 
-def permitted(ctx: commands.Context):
+async def permitted(ctx: commands.Context):
     config, data = dat()
     query = (
         f"SELECT `user_id` FROM `whitelisted_users` WHERE `user_id`='{ctx.author.id}'"
     )
-    res = bot.select(self=bot, sql=query)
+    res = await select(bot=bot, sql=query)
     # return ctx.author.id in data.get("permitted_users") or ctx.author.id in config.get(
     #     "admin_ids"
     # )
