40fb0eb0cd481396f3f04d800f3c5c2302020c49
EPFFORCE <38656336+mouncg@users.noreply.github.com>
2020-04-23 18:19:32 +0100

    ree

--- utils/checks.py
+++ utils/checks.py
@@ -1,5 +1,6 @@
 import json
 from discord.ext import commands
+from main import NitroBot as bot
 
 
 def dat():
@@ -12,9 +13,17 @@ def dat():
 
 def permitted(ctx: commands.Context):
     config, data = dat()
-    return ctx.author.id in data.get("permitted_users") or ctx.author.id in config.get(
-        "admin_ids"
+    query = (
+        f"SELECT `user_id` FROM `whitelisted_users` WHERE `user_id`='{ctx.author.id}'"
     )
+    res = await bot.select(sql=query)
+    # return ctx.author.id in data.get("permitted_users") or ctx.author.id in config.get(
+    #     "admin_ids"
+    # )
+    if res:
+        return True
+    else:
+        return False
 
 
 def running(ctx: commands.Context):
