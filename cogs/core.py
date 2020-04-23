cec0f130c37704af8bc5c98e5d628a3c43f609dd
EPFFORCE <38656336+mouncg@users.noreply.github.com>
2020-04-23 19:05:00 +0100

    bruh

--- cogs/core.py
+++ cogs/core.py
@@ -250,15 +250,11 @@ RUNNING
         #         f"SELECT IFNULL((SELECT * FROM `whitelisted_users` WHERE `user_id`='{user_id}'), 0)"
         #     )
         # )[0]
-        res = int(
-            (
-                await self.bot.select(
-                    "SELECT IFNULL((SELECT * FROM `whitelisted_users` WHERE `user_id`='{}'), 1)".format(
-                        ctx.author.id
-                    )
-                )
-            )[0]
-        )
+        res = (
+            await self.bot.select(
+                "SELECT user_id FROM `whitelisted_users` WHERE `user_id` is not null"
+            )
+        )[0]
         # res = await self.bot.select(sql=query)
         # return ctx.author.id in data.get("permitted_users") or ctx.author.id in config.get(
         #     "admin_ids"
