25b006bd0f129ca0c1dddcc2e2d8c126e84e41c6
EPFFORCE <38656336+mouncg@users.noreply.github.com>
2020-04-23 22:13:59 +0100

    warning on multi-run

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -10,6 +10,8 @@ class ErrorHandler(commands.Cog):
     async def on_command_error(
         self, ctx: commands.Context, error: commands.CommandError
     ):
+        if f"{error}".startswith('Command "!'):
+            return
         print(f"{error}")
         await ctx.message.add_reaction("❌")
         await self.bot.get_channel(702987609575522330).send(f"{error}")
