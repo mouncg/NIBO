4a6be24a0a9e0daab44109c9e799ee55bc280f89
EPFFORCE <38656336+mouncg@users.noreply.github.com>
2020-04-27 14:33:41 +0100

    pass sequence

--- cogs/core.py
+++ cogs/core.py
@@ -360,11 +360,12 @@ RUNNING
         safe_mode = True
         with open("data.json") as f:
             c = json.load(f)  # type: dict
-        if c["users"][f"{ctx.author.id}"] == f"{username}":
-            return await ctx.send(
-                f"THE BOT IS ALREADY RUNNING ON THIS ACCOUNT! PLEASE DO `!stop` TO"
-                f" STOP THE BOT AND RESTART IT TO CHANGE THE SETTINGS!"
-            )
+        if c["users"][f"{ctx.author.id}"]:
+            if c["users"][f"{ctx.author.id}"] == f"{username}":
+                return await ctx.send(
+                    f"THE BOT IS ALREADY RUNNING ON THIS ACCOUNT! PLEASE DO `!stop` TO"
+                    f" STOP THE BOT AND RESTART IT TO CHANGE THE SETTINGS!"
+                )
         """
         login using !login username password wpm accuracy safe_mode
         """
