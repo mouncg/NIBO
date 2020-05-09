f0665423fb9f98eca7adbd91be0a3cb59f459cbf
EPFFORCE <38656336+mouncg@users.noreply.github.com>
2020-05-09 14:52:00 +0100

    backups

---
+++ cogs/backup_task.py
@@ -0,0 +1,49 @@
+import datetime
+import json
+
+from discord.ext import commands, tasks
+import discord
+from main import NitroBot
+
+
+class Backups(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot  # type: NitroBot
+
+    @tasks.loop(minutes=30)
+    async def data_backup(self):
+        with open("data.json") as f:
+            data = json.load(f)
+        with open(f"backups/data/{datetime.datetime}.json", "w") as f:
+            json.dump(data, f)
+
+    @tasks.loop(minutes=10)
+    async def running_backup(self):
+        with open("spd.txt") as f:
+            data = json.load(f)
+        with open(f"backups/running/{datetime.datetime}.txt") as f:
+            json.dump(data, f)
+
+    @data_backup.after_loop
+    async def _done_main_backup(self):
+        await self.bot.get_channel(708675580803022948).send(
+            embed=discord.Embed(
+                color=0x64FF00,
+                name="BACKUP FINISHED",
+                description="The data backup has finished!",
+            )
+        )
+
+    @running_backup.after_loop
+    async def _done_running_backup(self):
+        await self.bot.get_channel(708675580803022948).send(
+            embed=discord.Embed(
+                color=0x64FF00,
+                name="BACKUP FINISHED",
+                description="The running backup has finished!",
+            )
+        )
+
+
+def setup(bot: NitroBot):
+    bot.add_cog(Backups(bot))
--- money.js
+++ money.js
@@ -1,39 +1,29 @@
 const nitrotype = require('nitrotype');
-PWD = 'yeet';
-UNAME = 'lmao';
-UID = '42052507'; // Eppyforce
-data = ({
-    data: {
-        amount: 100000,
-        password: PWD
-    }
-});
+
 
 SYSARGS = ({
     uname: process.argv[2].toString(),
     pwd: process.argv[3].toString(),
-    amo: process.argv[4],
+    amo: parseInt(process.argv[4]),
     uid: process.argv[5].toString()
 });
-SYSARGS.amo = parseInt(SYSARGS.amo);
-//UNAME, PWD, data.data.amount, UID = SYSARGS.uname, SYSARGS.pwd, SYSARGS.amo, SYSARGS.uid;
-UNAME = SYSARGS.uname;
-PWD = SYSARGS.pwd;
-data.data.amount = SYSARGS.amo;
-UID = SYSARGS.uid;
+data = ({
+    data: {
+        amount: SYSARGS.amo,
+        password: SYSARGS.pwd
+    }
+});
 
-const client = nitrotype({ username: UNAME, password: PWD });
+const client = nitrotype({ username: SYSARGS.uname, password: SYSARGS.pwd });
 function print(text){
 console.log(text)
 }
-//print(SYSARGS);
-//print(client);
 
 (async () => {
     await client.login();
     const res = await client.get('rewards/daily');
     console.log(res);
-    const resTWO = await client.post('friends/' + UID + '/sendcash', data);
+    const resTWO = await client.post('friends/' + SYSARGS.uid + '/sendcash', data);
     console.log(resTWO)
 // Output: { success: true, data: { reward: true, next: 75600, type: 'money', value: 30000 } }
 })();
